4f94c7307b3885dba439ca0e2db4e5d8d0f40ebb
Michel <106674127+MichelZero@users.noreply.github.com>
2022-09-01 10:20:32 -0300

    up

--- src/main02.cpp
+++ src/main02.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+
+// variáveis globais e bibliotecas
+
+#include "Servo.h"
+
+int pinPot = A0;
+int pinServo = 6;
+int valorMotor = 0;
+int valorPot;
+
+
 void setup() {
   // put your setup code here, to run once:
+  motorServo.attach(pinServo);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  valorPot = analogRead(pinPot);
+  valorMotor = map(valorPot,0,1023,0,180);
+  motorServo.write(valorMotor);
+  Serial.println(valorMotor);
+  delay(100);
 }
